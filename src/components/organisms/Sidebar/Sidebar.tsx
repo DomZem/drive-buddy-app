30a18195b0d7a119f584805a3052de0cd4f2019e
Dominik Zemlik <dominik.zemlik.dev@gmail.com>
2023-04-01 00:04:58 +0200

    Create main searchbar

--- src/components/organisms/Sidebar/Sidebar.tsx
+++ src/components/organisms/Sidebar/Sidebar.tsx
@@ -2,7 +2,7 @@ import SidebarHeader from '@/components/molecules/SidebarHeader/SidebarHeader';
 import SidebarMenu from '@/components/molecules/SidebarMenu/SidebarMenu';
 
 const Sidebar = () => (
-  <aside className="col-start-1 col-end-2 row-span-full flex flex-col">
+  <aside className="col-start-1 col-end-2 flex flex-col row-span-full">
     <SidebarHeader />
     <SidebarMenu />
   </aside>
--- src/components/templates/MainTemplate/MainTemplate.tsx
+++ src/components/templates/MainTemplate/MainTemplate.tsx
@@ -1,14 +1,25 @@
 import Sidebar from '@/components/organisms/Sidebar/Sidebar';
 import { type FC } from 'react';
+import { HiSearch } from 'react-icons/hi';
 
 interface MainTemplateProps {
   children: React.ReactNode;
 }
 
 const MainTemplate: FC<MainTemplateProps> = ({ children }) => (
-  <div className="grid grid-cols-[3.5rem_1fr] md:grid-cols-[20rem_1fr] md:grid-rows-[4rem_1fr] grid-rows-[3.5rem_1fr] w-full h-screen text-sm md:text-base">
+  <div className="grid grid-cols-[3.5rem_1fr] md:grid-cols-[20rem_1fr] md:grid-rows-[4rem_1fr] grid-rows-[3.5rem_1fr] h-screen text-sm md:text-base text-rich-black">
     <Sidebar />
-    {children}
+    <div className="p-2 md:p-3 gap-x-2 row-start-1 row-end-2 h-full flex items-center">
+      <div className="flex-1 flex gap-x-2 items-center">
+        <HiSearch className="text-3xl" />
+        <input
+          className="py-1.5 px-2 text-xs md:text-sm max-w-lg w-full text-rich-black"
+          type="text"
+          placeholder="Search some users, cars..."
+        />
+      </div>
+    </div>
+    <main className="bg-silver p-6 overflow-y-auto">{children}</main>
   </div>
 );
 
