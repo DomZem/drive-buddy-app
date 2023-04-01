2992980b6d907a4183b5ffc48a16b6aa70eff3b0
Dominik Zemlik <dominik.zemlik.dev@gmail.com>
2023-04-01 03:23:31 +0200

    Complete style instructor card

--- src/components/templates/MainTemplate/MainTemplate.tsx
+++ src/components/templates/MainTemplate/MainTemplate.tsx
@@ -7,19 +7,19 @@ interface MainTemplateProps {
 }
 
 const MainTemplate: FC<MainTemplateProps> = ({ children }) => (
-  <div className="grid grid-cols-[3.5rem_1fr] md:grid-cols-[20rem_1fr] md:grid-rows-[4rem_1fr] grid-rows-[3.5rem_1fr] h-screen text-sm md:text-base text-rich-black">
+  <div className="grid h-screen grid-cols-[3.5rem_1fr] grid-rows-[3.5rem_1fr] text-sm text-rich-black md:grid-cols-[18rem_1fr] md:grid-rows-[4rem_1fr] md:text-base">
     <Sidebar />
-    <div className="p-2 md:p-3 gap-x-2 row-start-1 row-end-2 h-full flex items-center">
-      <div className="flex-1 flex gap-x-2 items-center">
+    <div className="row-start-1 row-end-2 flex h-full items-center gap-x-2 p-2 md:p-3">
+      <div className="flex flex-1 items-center gap-x-2">
         <HiSearch className="text-3xl" />
         <input
-          className="py-1.5 px-2 text-xs md:text-sm max-w-lg w-full text-rich-black"
+          className="w-full max-w-lg px-2 py-1.5 text-xs text-rich-black md:text-sm"
           type="text"
           placeholder="Search some users, cars..."
         />
       </div>
     </div>
-    <main className="bg-slate-gray p-2 overflow-y-auto">{children}</main>
+    <main className="overflow-y-auto bg-slate-gray p-2 md:p-4">{children}</main>
   </div>
 );
 
--- src/routes/Instructors.tsx
+++ src/routes/Instructors.tsx
@@ -1,24 +1,64 @@
 import userImg from '@/assets/img/user.jpg';
-import { BiShow } from 'react-icons/bi';
-import { MdDelete } from 'react-icons/md';
+import { BiCategory, BiShow } from 'react-icons/bi';
+import { BsPhoneFill } from 'react-icons/bs';
+import { MdDelete, MdOutlinePhoneAndroid } from 'react-icons/md';
 
 const Instructors = () => (
-  <div className="max-w-5xl">
-    <ul className="grid gap-3">
-      <li>
-        <div className="p-3 bg-white rounded-t-lg flex gap-x-3">
-          <img className="w-11 h-11 rounded-full" src={userImg} alt="dominik zemlik profile" />
-          <div className="flex flex-col gap-y-1">
-            <h2 className="font-semibold">Dominik Zemlik</h2>
-            <p className="text-xs">Licences - A, A1, B1, B2, B3</p>
+  <div className="mx-auto max-w-7xl">
+    <ul className="grid grid-cols-[repeat(auto-fit,minmax(15rem,1fr))] gap-3 md:grid-cols-[repeat(auto-fit,minmax(15rem,1fr))] md:gap-4">
+      <li className="flex flex-col">
+        <div className="flex flex-1 flex-col gap-3 rounded-t-lg bg-white p-3">
+          <div className="flex items-center gap-3 md:flex-col">
+            <img className="h-12 w-12 rounded-full md:h-20 md:w-20" src={userImg} alt="dominik zemlik profile" />
+            <h2 className="font-semibold md:text-lg">Luke Skywalker</h2>
           </div>
+          <ul className="flex flex-col items-start gap-y-3">
+            <li className="flex items-center gap-x-2 text-xs md:text-sm">
+              <BiCategory className="text-base md:text-xl" />
+              Licences - A1, A2, A, B1, B2, C+E
+            </li>
+            <li className="flex items-center gap-x-2 text-xs md:text-sm">
+              <BsPhoneFill className="text-base md:text-xl" />
+              492 483 023
+            </li>
+          </ul>
         </div>
         <div className="flex">
-          <button className="flex-1 flex items-center justify-center gap-x-2 p-2 text-xs font-medium bg-rich-black text-white rounded-bl-lg">
-            <BiShow className="text-base" /> View
+          <button className="flex flex-1 items-center justify-center gap-x-2 rounded-bl-lg bg-rich-black p-2 text-xs font-medium text-white duration-200 hover:bg-[#000605] md:p-3 md:text-sm">
+            <BiShow className="text-base md:text-xl" />
+            View
           </button>
-          <button className="flex-1 flex items-center justify-center gap-x-2 p-2 text-xs font-medium bg-red text-white rounded-br-lg">
-            <MdDelete className="text-base" />
+          <button className="flex flex-1 items-center justify-center gap-x-2 rounded-br-lg bg-red p-2 text-xs font-medium text-white duration-200 hover:bg-[#ba0404] md:p-3 md:text-sm">
+            <MdDelete className="text-base md:text-xl" />
+            Delete
+          </button>
+        </div>
+      </li>
+
+      <li className="flex flex-col">
+        <div className="flex flex-1 flex-col gap-3 rounded-t-lg bg-white p-3">
+          <div className="flex items-center gap-3 md:flex-col">
+            <img className="h-12 w-12 rounded-full md:h-20 md:w-20" src={userImg} alt="dominik zemlik profile" />
+            <h2 className="font-semibold md:text-lg">Anakin Skywalker</h2>
+          </div>
+          <ul className="flex flex-col items-start gap-y-3">
+            <li className="flex items-center gap-x-2 text-xs md:text-sm">
+              <BiCategory className="text-base md:text-xl" />
+              Licences - A1, A2, A, B1, B2, C+E
+            </li>
+            <li className="flex items-center gap-x-2 text-xs md:text-sm">
+              <MdOutlinePhoneAndroid className="text-base md:text-xl" />
+              823 383 922
+            </li>
+          </ul>
+        </div>
+        <div className="flex">
+          <button className="flex flex-1 items-center justify-center gap-x-2 rounded-bl-lg bg-rich-black p-2 text-xs font-medium text-white duration-200 hover:bg-[#000605] md:p-3 md:text-sm">
+            <BiShow className="text-base md:text-xl" />
+            View
+          </button>
+          <button className="flex flex-1 items-center justify-center gap-x-2 rounded-br-lg bg-red p-2 text-xs font-medium text-white duration-200 hover:bg-[#ba0404] md:p-3 md:text-sm">
+            <MdDelete className="text-base md:text-xl" />
             Delete
           </button>
         </div>
