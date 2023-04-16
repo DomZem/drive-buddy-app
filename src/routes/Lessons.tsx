a459f03d96e2968c9d01f2210c0860dcb1bc5e46
Dominik Zemlik <dominik.zemlik@microsoft.wsei.edu.pl>
2023-04-16 16:52:30 +0200

    Add modal to delete lesson

--- src/routes/Lessons.tsx
+++ src/routes/Lessons.tsx
@@ -1,10 +1,13 @@
 import DetailsList from '@/components/molecules/DetailsList/DetailsList';
+import DeleteItemModal from '@/components/organisms/DeleteItemModal/DeleteItemModal';
 import SearchCreateBar from '@/components/organisms/SearchCreateBar/SearchCreateBar';
 import CardItemTemplate from '@/components/templates/CardItemTemplate/CardItemTemplate';
+import Modal from '@/components/templates/Modal/Modal';
+import useModal from '@/components/templates/Modal/useModal';
 import PageTemplate from '@/components/templates/PageTemplate/PageTemplate';
 import { db } from '@/firebase/config';
-import { type LessonType } from '@/types';
-import { collection, doc, getDoc, onSnapshot } from '@firebase/firestore';
+import { ModalType, type LessonType } from '@/types';
+import { collection, deleteDoc, doc, onSnapshot } from '@firebase/firestore';
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
@@ -25,6 +28,32 @@ const container = {
 
 const Lessons = () => {
   const [lessons, setLessons] = useState<LessonType[]>([]);
+  const [currentLesson, setCurrentLesson] = useState<LessonType>(lessons[0]);
+  const [currentModal, setCurrentModal] = useState<ModalType>('update-create');
+
+  const { isOpen, handleOpenModal, handleCloseModal } = useModal();
+
+  const handleOpenItem = (modal: ModalType, currentItem: LessonType) => {
+    if (modal === 'delete') {
+      setCurrentModal('delete');
+    } else {
+      setCurrentModal('update-create');
+    }
+    setCurrentLesson(currentItem);
+    handleOpenModal();
+  };
+
+  const handleDeleteStudent = async () => {
+    const studentDoc = doc(db, 'lessons', currentLesson.id);
+    try {
+      await deleteDoc(studentDoc);
+      toast.success('The Lesson has been deleted');
+    } catch (e) {
+      toast.error('Something went wrong. The Lesson has not been deleted');
+    } finally {
+      handleCloseModal();
+    }
+  };
 
   useEffect(() => {
     const unsub = onSnapshot(
@@ -68,19 +97,7 @@ const Lessons = () => {
           animate="visible"
         >
           {lessons.map((lesson) => {
-            const { id, location, lessonType, courseCategory, avatar, date, carId } = lesson;
-            const docRef = doc(db, 'cars', carId);
-
-            void (async () => {
-              const docSnap = await getDoc(docRef);
-
-              if (docSnap.exists()) {
-                console.log('Document data:', docSnap.data());
-              } else {
-                // docSnap.data() will be undefined in this case
-                console.log('No such document!');
-              }
-            })();
+            const { id, location, lessonType, courseCategory, avatar, date } = lesson;
 
             const detailsList = [
               {
@@ -97,7 +114,7 @@ const Lessons = () => {
               },
               {
                 icon: FaCarAlt,
-                value: `Car`,
+                value: `Car:`,
               },
             ];
 
@@ -105,7 +122,9 @@ const Lessons = () => {
               <CardItemTemplate
                 title={`${lessonType} (${courseCategory}) - ${location}`}
                 imageSrc={avatar}
-                onDeleteItem={() => console.log('Item has been deleted!')}
+                onDeleteItem={() => {
+                  handleOpenItem('delete', lesson);
+                }}
                 onUpdateItem={() => console.log('Item has been updated!')}
                 key={id}
               >
@@ -115,6 +134,16 @@ const Lessons = () => {
           })}
         </motion.ul>
       ) : null}
+
+      <Modal isOpen={isOpen} onCloseModal={handleCloseModal}>
+        {currentModal === 'delete' && (
+          <DeleteItemModal
+            onCloseModal={handleCloseModal}
+            onDeleteItem={handleDeleteStudent}
+            boldText={` ${currentLesson.lessonType} (${currentLesson.courseCategory}) lesson in ${currentLesson.location} at ${currentLesson.date}`}
+          />
+        )}
+      </Modal>
     </PageTemplate>
   );
 };
